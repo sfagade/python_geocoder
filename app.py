2402340408dd6a98e59e1ca94434b8fc835affac
Samson <sfagade@gmail.com>
2020-10-18 15:31:34 +0100

    created functionality to fetch coordinates from address and save the values in a csv file

--- app.py
+++ app.py
@@ -10,27 +10,20 @@ def home():
     return render_template("index.html")
 
 
-@app.context_processor
-def get_current_user():
-    print("context called", data_records)
-    return {"data": data_records.to_html(classes='data')}
-
-
 @app.route("/success", methods=['POST'])
 def success():
-    global data_records
-
     if request.method == 'POST':
         csv_file = request.files['csv_file']
         csv_file.save(secure_filename("uploaded" + csv_file.filename))
 
         data_records = process_geo_coder("uploaded" + csv_file.filename)
-        return render_template("index.html", data="data_viewer.html")
+        return render_template("index.html", text=data_records.to_html(), data="data_viewer.html")
 
 
 @app.route("/download")
 def download():
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- upload_processor.py
+++ upload_processor.py
@@ -14,18 +14,22 @@ def process_geo_coder(file_name):
     pandas_data.columns = [x.lower() for x in pandas_data.columns]  # convert all headers to lowercase
     if 'address' in pandas_data.columns:  # we only want to continue if there is an address column in the file
         geo_locator = Nominatim(user_agent="example app")
+        pandas_data['coordinates'] = pandas_data['address'].apply(geo_locator.geocode)
+        pandas_data['latitude'] = pandas_data['coordinates'].apply(lambda x: x.latitude if x is not None else None)
+        pandas_data['longitude'] = pandas_data['coordinates'].apply(lambda x: x.longitude if x is not None else None)
+        pandas_data.drop('coordinates', 1)
+        """
         lon_data = []
         lat_data = []
-        """geocode = RateLimiter(geo_locator.geocode, min_delay_seconds=1)
-        pandas_data['location'] = pandas_data['address'].apply(geocode)"""
+       
         for index, row in pandas_data.iterrows():
-            address = row['address']+","+row['city']+","+row['state']+","+row['country']
+            address = row['address'] + "," + row['city'] + "," + row['state'] + "," + row['country']
             location = geo_locator.geocode(address).raw
             lon_data.append(location.get('lon'))
             lat_data.append(location.get('lat'))
 
-        pandas_data.insert(len(pandas_data.columns),"lat", lat_data, True)
-        pandas_data.insert(len(pandas_data.columns), "lon", lon_data, True)
+        pandas_data.insert(len(pandas_data.columns), "lat", lat_data, True)
+        pandas_data.insert(len(pandas_data.columns), "lon", lon_data, True) """
         pandas_data.to_csv("output_data.csv")
         return pandas_data
     else:
